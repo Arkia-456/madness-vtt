6064c07502228e46321faf3d4a61054ab7b8fe40
Arkia <145221230+Arkia-456@users.noreply.github.com>
2023-11-09 00:32:14 +0100

    Character: Add secondary stats calculation

--- module/MadnessActor.js
+++ module/MadnessActor.js
@@ -6,6 +6,8 @@ export default class MadnessActor extends Actor {
     super.prepareData();
     const stats = this.system.stats;
     this._calculateTotalPrimaryStats(stats);
+    this._calculateSecondaryStats(stats);
+    this._calculateTotalSecondaryStats(stats);
   }
 
   _calculateTotalPrimaryStats(stats) {
@@ -14,6 +16,66 @@ export default class MadnessActor extends Actor {
         stats[key].total = MadnessUtils.convertToInt(stats[key].base) + MadnessUtils.convertToInt(stats[key].mod)
       }
     }
+  }
+
+  _calculateSecondaryStats(stats) {
+    stats.parryDamageReduction = {
+      base: this._calculateParryDamageReduction(MadnessUtils.convertToInt(stats.constitution.total))
+    }
+    stats.critRate = {
+      base: this._calculateCritRate(MadnessUtils.convertToInt(stats.dexterity.total))
+    }
+    stats.evadeRate = {
+      base: this._calculateEvadeRate(MadnessUtils.convertToInt(stats.agility.total))
+    }
+    stats.maxMoveDistance = {
+      base: this._calculateMaxMoveDistance(MadnessUtils.convertToInt(stats.agility.total))
+    }
+    stats.initiativeBonus = {
+      base: this._calculateInitiativeBonus(MadnessUtils.convertToInt(stats.agility.total))
+    }
+    stats.maxEquipmentWeight = {
+      base: this._calculateMaxEquipmentWeight(MadnessUtils.convertToInt(stats.strength.total))
+    }
+    stats.maxWeight = {
+      base: this._calculateMaxWeight(MadnessUtils.convertToInt(stats.strength.total))
+    }
+  }
+
+  _calculateTotalSecondaryStats(stats) {
+    for (const [key, value] of Object.entries(stats)) {
+      if (!value.primary) {
+        stats[key].total = MadnessUtils.convertToInt(stats[key].base) + MadnessUtils.convertToInt(stats[key].mod)
+      }
+    }
+  }
+
+  _calculateParryDamageReduction(value) {
+    return 50 + ( 2 * value );
+  }
+
+  _calculateCritRate(value) {
+    return 5 + value;
+  }
+
+  _calculateEvadeRate(value) {
+    return 50 + ( 2 * value );
+  }
+
+  _calculateMaxMoveDistance(value) {
+    return 3 + value;
+  }
+
+  _calculateInitiativeBonus(value) {
+    return value;
+  }
+
+  _calculateMaxEquipmentWeight(value) {
+    return value * 5;
+  }
+  
+  _calculateMaxWeight(value) {
+    return value * 10;
   }
 
 }
