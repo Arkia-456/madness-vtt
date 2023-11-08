39f8d8a4d41231fc5805dbeec1c02a59eeee4b1d
Arkia <145221230+Arkia-456@users.noreply.github.com>
2023-11-09 00:27:58 +0100

    Character: Move weapon items to partial template

--- madness.js
+++ madness.js
@@ -1,10 +1,19 @@
 import MadnessCharacterSheet from './module/sheets/MadnessCharacterSheet.js';
 import MadnessItemSheet from './module/sheets/MadnessItemSheet.js';
 
+async function preloadHandlebarsTemplates() {
+  const templatePaths = [
+    'systems/madness/templates/partials/weapon-card.hbs'
+  ];
+  return loadTemplates(templatePaths);
+}
+
 Hooks.once('init', () => {
   Actors.unregisterSheet('core', ActorSheet);
   Actors.registerSheet('madness', MadnessCharacterSheet, { makeDefault: true });
 
   Items.unregisterSheet('core', ItemSheet);
   Items.registerSheet('madness', MadnessItemSheet, { makeDefault: true });
+
+  preloadHandlebarsTemplates();
 });
