ebaa0de3825e1dc1afce8d8b147916278df891b6
Arkia <145221230+Arkia-456@users.noreply.github.com>
2023-11-10 20:25:40 +0100

    Skill: Rework skill sheet and card, add requirements and damage dice

--- module/sheets/MadnessItemSheet.js
+++ module/sheets/MadnessItemSheet.js
@@ -4,10 +4,17 @@ export default class MadnessItemSheet extends ItemSheet {
     return `systems/madness/templates/sheets/${this.item.type}-sheet.hbs`;
   }
 
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ['madness', 'sheet']
+    });
+  }
+
   getData() {
     const baseData = super.getData();
     console.log(baseData);
     return {
+      user: game.user,
       item: baseData.item,
       system: baseData.item.system,
       config: CONFIG.madness
