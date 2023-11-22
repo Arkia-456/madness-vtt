b6080e45743344a1b4ca146b52053894031192c4
Arkia <145221230+Arkia-456@users.noreply.github.com>
2023-11-22 20:09:59 +0100

    Dice: Change crit mod variable name

--- module/utils/MadnessDice.js
+++ module/utils/MadnessDice.js
@@ -57,12 +57,12 @@ export default class MadnessDice {
     const messageTemplate = 'systems/madness/templates/chat/critical-check.hbs';
     const rollFormula = madness.formulas.roll.default;
     const rollResult = await new Roll(rollFormula).roll({ async: true });
-    let mod = 0;
+    let critMod = 0;
     if (item) {
-      mod += item.getActiveEffectCritBonus();
+      critMod += item.getActiveEffectCritBonus();
     }
-    const isCriticalSuccess = rollResult.total <= actor.system.stats.critRate.total + mod;
     const isCriticalFail = rollResult.total > madness.thresholds.criticalFail;
+    const isCriticalSuccess = rollResult.total <= actor.system.stats.critRate.total + critMod;
     await MadnessDice.toCustomMessage(actor, rollResult, messageTemplate, {
       isCriticalSuccess: isCriticalSuccess,
       isCriticalFail: isCriticalFail
